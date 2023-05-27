652ed1b7c26f59b90378c866036a58e9049161fb
Justin-Barlowe <jbarlowe@my365.bellevue.edu>
2023-05-27 12:46:27 -0400

    Fixing dark mode issues

--- theme.js
+++ theme.js
@@ -5,17 +5,13 @@
 ; Description: Theme JavaScript page
  */
 
-document.getElementById("icon-text").innerHTML = "Light Mode"; //Sets Light mode text display as default for page.
-
+document.getElementById("icon-text").innerHTML = localStorage.getItem("iconText") || "Light Mode"; // Set the initial theme text display.
 
 function toggleMode(x) {
-  //Function created to theme toggle.
-
   let colorTheme = document.body.classList;
   let iconMode = x.classList;
 
   if (colorTheme.value === "light-theme") {
-    // Check which theme is currently selected.
     localStorage.setItem("mode", "dark-theme"); // Set dark theme.
     localStorage.setItem("iconMode", "fa-toggle-on");
     localStorage.setItem("iconText", "Dark Mode");
@@ -27,25 +23,8 @@ function toggleMode(x) {
 
   colorTheme.value = localStorage.getItem("mode"); // Updates theme based on the stored mode.
   iconMode.value = `fa ${localStorage.getItem("iconMode")} pull-right`; // Updates icon class.
-  document.getElementById("icon-text").innerHTML =
-    localStorage.getItem("iconText"); //Update text display based on set theme.
+  document.getElementById("icon-text").innerHTML = localStorage.getItem("iconText"); // Update text display based on set theme.
 }
 
-function setDefaultTheme() {
-  // These variables capture elements from the DOM
-  const bodyClass = document.body;
-  const iconElement = document.getElementById("icon-mode");
-  const iconText = document.getElementById("icon-text");
+setDefaultTheme(); // Call the function to set the default theme.
 
-  // Used to set the local storage if no other values are found
-  bodyClass.classList.value = localStorage.getItem("mode") || "light-theme";
-  iconElement.classList.add(
-    localStorage.getItem("iconMode") || "fa-toggle-off"
-  );
-  iconText.innerHTML = localStorage.getItem("iconText") || "Light Mode";
-}
-
-// set the them on the body
-function setSelectedTheme() {
-  document.body.classList.value = localStorage.getItem("mode") || "light-theme";
-}
