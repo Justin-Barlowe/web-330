62cc6dfc1a9b10b0c554a78d6b82d574bcda30fa
Justin-Barlowe <jbarlowe@my365.bellevue.edu>
2023-06-01 09:33:05 -0400

    Assignment 2.2 in progress

--- week-2/barlowe-palindrome.js
+++ week-2/barlowe-palindrome.js
@@ -3,40 +3,44 @@
 // Date: 5/31/2023
 // Description: JS Page for Assignment 2.2
 
+
+
 function getTodaysDate() {
-    const options = { weekday: 'long', year: 'numeric', day: 'numeric' };
-    const date = new Date().toLocaleDateString('en-US', options);
-    return date;
+  return new Date().toLocaleDateString();
 }
 
-function getLength(str) {
-    return str.length;
+function getLength(word) {
+  return word.length;
 }
 
-function reverse(str) {
-    const charArray = str.splint('');
-    const reversedArray = charArray.reverse();
-    const reversedStr = reversedArray.join('');
-    return reversedStr;
+function reverse(word) {
+  return word.split("").reverse().join("");
 }
 
-function isPalindrome(str) {
-    const reversedStr = reverse(str);
-    return reversedStr === str;
+function isPalindrome(word) {
+  if (reverse(word) === word) return true;
+  return false;
 }
 
-document.getElementById('btnCheckPhrase').onClick = isPalindrome();
+document.getElementById("btnCheckPhrase").onclick = () => {
+  const txtPhrase = document.getElementById("txtPhrase").value;
+  const assignResults = document.getElementById("assign-results");
+  const assignResultsText = document.getElementById("assign-results-text");
+  const today = getTodaysDate();
+  const len = getLength(txtPhrase);
+  const reversedPhrase = reverse(txtPhrase);
 
-document.getElementById('assign-results-text').innerHTML
+//Build string to display output in text results. 
+  let results =  `Date: ${today} <br>
+                  Original Phrase: ${txtPhrase} <br>
+                  Reversed Phrase: ${reversedPhrase} <br>
+                  Phrase Length: ${len}`;
 
-var txtPhrase = document.getElementById("txtPhrase").value
+  // modify the assign-results-header div to display the header string
+  assignResultsText.innerHTML = results;
 
-var assignResults = document.getElementById("assignResults");
-
-var today = getTodaysDate();
-
-var len = getLength(txtPhrase);
-
-
-assignResults.innerHTML = "Phrase: " + txtPhrase + "<br>" + "Today's Date: " + today + "<br>" +
-                            "Length: " + len + "<br>" + "Reversed Phrase: " + reversedPhrase;
+  // Check input string to determine message to display. 
+  if (isPalindrome(txtPhrase))
+    return (assignResults.innerHTML = `${txtPhrase} <b><u>is</u></b> a palindrome!`); // Displays message if palindrome is true
+  return (assignResults.innerHTML = `${txtPhrase} <b><u>is not</u></b> a palindrome!`); // Displays message if palindrome is false
+};
